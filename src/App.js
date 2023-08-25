a5c60b0d987c8067856b3564baeb8c7b4f455df0
kevin <53587009+980k@users.noreply.github.com>
2023-08-24 18:35:25 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <h2>First React App On a Live Server!</h2>
+    </>
+  );
+}
+
+export default App;
